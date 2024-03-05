9ac07951fe3f161752662ed9ab1a69dda6017b99
Alper Şallı <alpersallidgb@gmail.com>
2024-03-05 17:45:11 +0300

    wificredential
    wificredential with rpi

--- MobilApp/app/src/main/java/com/example/loginapp/SignInActivity.kt
+++ MobilApp/app/src/main/java/com/example/loginapp/SignInActivity.kt
@@ -1,11 +1,13 @@
 package com.example.loginapp
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginapp.databinding.ActivitySignInBinding
+import com.example.loginapp.databinding.WifiCredentialBinding
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import org.json.JSONObject
@@ -17,11 +19,19 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     private lateinit var btn: ActivitySignInBinding
     private val client = OkHttpClient()
+    private lateinit var wifi : WifiCredentialBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.wifiCredentialBtn.setOnClickListener {
+            val intent = Intent(this, WifiCredentialActivity::class.java)
+            startActivity(intent)
+        }
+
 
         binding.textView1.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
